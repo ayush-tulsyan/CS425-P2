a97e0bcdc3c771f3abafa4a57d34a02791909b97
Ayush Tulsyan <ayushtuls.64@gmail.com>
2016-08-31 20:00:23 +0000

    Completed the proxy's server end, Started the client end and completed it

--- proxy.c
+++ proxy.c
@@ -4,12 +4,157 @@
 #include <sys/socket.h>
 
 #define Buf_Size_Initial 4096
+#define Ip_addr 1
+#define Server_Name_addr 2
 
-void dealClient(int sockid) {
-    char buffer[Buf_Size_Initial];
-    int flag, check, ret;
-    ret = recv(sockid, buffer, Buf_Size_Initial, 0);
+int sendErrorReply(int sockid, int errorID) {
 
+    return 0;
+}
+
+/*char *getIPbyname(ParsedRequest *req) {
+    int flag;
+    flag = hostAddressType(req);
+    if (flag == Server_Name_addr) {
+        struct hostent* destInfo = gethostbyname(req->host);
+        if (destInfo != NULL) {
+
+        }
+    } else if (flag == Ip_addr) {
+        return req->host;
+    }
+    return NULL;
+}*/
+
+int EstablishConnection(ParsedRequest *req, int *sockid) {
+    if (req && req->host && req->port) {
+
+        struct hostent* destInfo = gethostbyname(req->host);
+
+        *sockid = socket(AF_INET, SOCK_STREAM, 0);
+
+        if (destInfo == NULL || *sockid < 0) {
+            return -1;
+        }
+
+        int destPort = atoi(req->port);
+
+        struct sockaddr_in serv_addr;
+
+        memset(&serv_addr, 0, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        bcopy((char *)destInfo->h_addr, (char *)&serv_addr.sin_addr.s_addr, destInfo->h_length);
+        serv_addr.sin_port = htons(destPort);
+
+        if (connect(*sockid, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+            return -1;
+        }
+
+    } else {
+        return -1;
+    }
+
+    return 0;
+}
+
+int RecvFromSock(int SockId, char *buffer) {
+    int len = 0, size = Buf_Size_Initial;
+    if (buffer == NULL) 
+        return -1;
+
+    len = recv(SockId, buffer, size, 0);
+    while (len == size) {
+        buffer = (char *)realloc(buffer, 2 * size * sizeof(char));
+        len += recv(SockId, buffer + len, size, 1);
+        size *= 2;
+    }
+    if(len <= 0)
+        return -1;
+    else
+        buffer[len] = 0;
+
+    return len + 1;
+}
+
+int dealClient(int SockId) {
+    char *buffer, *send_buf;
+    int  destSockId, ret;
+    size_t len;
+
+    buffer = (char *)malloc(Buf_Size_Initial * sizeof(char));
+    len = RecvFromSock(SockId, buffer);
+    if (len <= 0) {
+        free(buffer);
+        sendErrorReply(SockId, 500);
+        return -1;
+    }
+
+    //Create a ParsedRequest to use. This ParsedRequest
+    //is dynamically allocated.
+    ParsedRequest *req = ParsedRequest_create();
+    ret = ParsedRequest_parse(req, buffer, len);
+    if (ret < 0) {
+        sendErrorReply(SockId, 500);
+        return -1;
+    }
+    free(buffer);
+
+    if(EstablishConnection(req, &destSockId) < 0) {
+        sendErrorReply(SockId, 500);
+        ParsedRequest_destroy(req);
+        return -1;
+    }
+
+    if (ParsedHeader_set(req, "Connection", "close") < 0) {
+        sendErrorReply(SockId, 500);
+        ParsedRequest_destroy(req);
+        return -1;
+    }
+
+    if (ParsedHeader_set(req, "Host", req->host) < 0) {
+        ParsedRequest_destroy(req);
+        sendErrorReply(SockId, 500);
+        return -1;
+    }
+
+    len = ParsedRequest_totalLen(req);
+
+    send_buf = (char *)malloc((len + 1) * sizeof(char));
+    if (send_buf == NULL) {
+        ParsedRequest_destroy(req);
+        sendErrorReply(SockId, 500);
+        return -1;
+    }
+
+    if (ParsedRequest_unparse(req, send_buf, len + 1) < 0) {
+        free(send_buf);
+        sendErrorReply(SockId, 500);
+        return -1;
+    }
+    ParsedRequest_destroy(req);
+
+    if (send(destSockId, send_buf, len + 1, 0) <= 0) {
+        sendErrorReply(SockId, 500);
+        return -1;
+    }
+    free(send_buf);
+
+    buffer = (char *)malloc(Buf_Size_Initial * sizeof(char));
+    len = RecvFromSock(destSockId, buffer);
+    if (len <= 0) {
+        sendErrorReply(SockId, 500);
+        free(buffer);
+        return -1;
+    }
+
+    if (send(SockId, buffer, len, 0) <= 0) {
+        sendErrorReply(SockId, 500);
+        return -1;
+    }    
+    free(buffer);
+    ParsedRequest_destroy(req);
+
+    return 0;
 }
 
 int main(int argc, char * argv[]) {
@@ -20,10 +165,9 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-	int sockid, newsockid, serverport, clientIP[4];
+	int sockid, newsockid, serverport, clientIP[4], i;
     struct sockaddr_in serv_addr, cli_addr;
-    char buffer[4096], streamlen[5];
-    int  n, m, i, size;
+    // int  n, m, i, size;
     unsigned int addrLen, client1, client2;
 
     // declaring socket
@@ -55,6 +199,7 @@ int main(int argc, char * argv[]) {
         return 0;
     }
     addrLen = sizeof(cli_addr);
+    FILE *fp = fopen("proxy.log", "a");
 
     while (1) {
         // Accept connection from the client 
@@ -68,7 +213,7 @@ int main(int argc, char * argv[]) {
 
         //calculates client IP in little endian
         client1 = cli_addr.sin_addr.s_addr, i = 3;
-        printf("Client connected from IP: ");
+        fprintf(fp, "Client connected from IP: ");
         while (client1 > 0) {
             client2 = client1 / (1 << 8*(i));
             clientIP[i] = client2;
@@ -77,9 +222,9 @@ int main(int argc, char * argv[]) {
         }
 
         /* Prints Client's IP Address */
-        /*for (int i = 0; i < 4; ++i) {
-            printf("%d%c", clientIP[i], (i == 3)?'\n':'.');
-        }*/
+        for (int i = 0; i < 4; ++i) {
+            fprintf(fp, "%d%c", clientIP[i], (i == 3)?'\n':'.');
+        }
 
         if (fork() == 0) {
             dealClient(newsockid);
